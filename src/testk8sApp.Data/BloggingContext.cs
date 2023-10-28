cf2ad6fd05ba2d212aca2c85395554c24ae486b4
Gerardo Hernandez <gerardo@hernandez.perales.dev>
2023-10-27 22:39:36 -0600

    add postgres image for the service

--- src/testk8sApp.Data/BloggingContext.cs
+++ src/testk8sApp.Data/BloggingContext.cs
@@ -6,15 +6,17 @@ public class BloggingContext : DbContext
 {
     public BloggingContext(DbContextOptions<BloggingContext> options) :base(options){}
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql();
-    }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseSerialColumns();
-        modelBuilder.Entity<ProofOfLife>().HasData(new List<ProofOfLife>() { new() { Id = Guid.Parse("00000000-0000-0000-0000-000000000001") } });
+        modelBuilder.Entity<ProofOfLife>()
+            .HasData(new List<ProofOfLife>()
+            {
+                new()
+                {
+                    Id = Guid.Parse("00000000-0000-0000-0000-000000000001")
+                }
+            });
     }
     
     public DbSet<Blog> Blogs { get; set; }
